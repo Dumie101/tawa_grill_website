a496b9c378f2c5a2f6307b085191e50dce0cd9c2
Ichiro Banskota <36200378+Dumie101@users.noreply.github.com>
2024-06-02 21:13:35 -0300

    <5>
    Server Fix

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
-    return render_template("index.html")
+    return "Hello world!"
 
 if (__name__ == '__main__'):
-    app.run(debug=True)
+    app.run()
