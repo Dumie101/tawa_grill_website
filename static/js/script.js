d9075557da5c2d37834aa7920b8e6bc321146773
Ichiro Banskota <36200378+Dumie101@users.noreply.github.com>
2024-06-07 00:31:20 -0300

    <10> start of nav bar
    started making nav bar

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,13 @@
 
 'use strict';
 
+// This function will be called when the HTML Content is loaded in 
 document.addEventListener("DOMContentLoaded", function() {
+    // Selects the preloader element with the data preloaded //
     const preloader = document.querySelector("[data-preload]");
-
+    // Checks the preloader element //
     if (preloader) {
+        // Event listenr that waits for everything to load, if it loads then it addes the loaded //
         window.addEventListener("load", function() {
             preloader.classList.add("loaded");
             document.body.classList.add("loaded");
@@ -13,3 +16,5 @@ document.addEventListener("DOMContentLoaded", function() {
         console.error('Preloader element not found');
     }
 });
+
+
