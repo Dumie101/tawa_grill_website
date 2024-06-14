f1db259084fbf3829c47853cdfd1a81196d5c3fb
Ichiro Banskota <36200378+Dumie101@users.noreply.github.com>
2024-06-14 14:37:53 -0300

    <2> Nav bar creation
    Buttons created on nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,14 +4,23 @@ import './Navbar.css';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
+import images from '../../constants/images';
 import './Navbar.css'
 
 
 const Navbar = () => (
   <div>
     <nav className='navbar'>
-      Navbar
-    </nav>
+      <div className='navbar-logo'>
+        <img src={images.tawalogo} alt="tawa grill logo" />
+      </div>
+      <ul className="navbar-links">
+        <li className="p__opensans"><a href='#home'>Home</a></li>
+        <li className="p__opensans"><a href='#about'>About</a></li>
+        <li className="p__opensans"><a href='#menu'>Menu</a></li>
+        <li className="p__opensans"><a href='#contact'>Contact</a></li>
+      </ul>
+    </nav> 
   </div>
 );
 
