e69c7c1dde3fe95ffb8fbcfa1a17b1d404686547
Ichiro Banskota <36200378+Dumie101@users.noreply.github.com>
2024-06-14 13:26:55 -0300

    <1> Restarted with react
    restarted the website with react framework

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Navbar.css';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+
+import './Navbar.css'
+
+
+const Navbar = () => (
+  <div>
+    <nav className='navbar'>
+      Navbar
+    </nav>
+  </div>
+);
+
+export default Navbar;
---
+++ src/constants/images.js
@@ -0,0 +1,12 @@
+
+import award01 from '../assets/award01.png';
+import award02 from '../assets/award02.png';
+import award03 from '../assets/award03.png';
+import award05 from '../assets/award05.png';
+
+export default {
+  award01,
+  award02,
+  award03,
+  award05,
+};
