c39692b902d24d77793ba77876f31464ededa450
Ichiro Banskota <36200378+Dumie101@users.noreply.github.com>
2024-06-28 18:29:45 -0300

    <11> About us
    About us fully finished

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
-
 import './AboutUs.css';
 import { images } from '../../constants';
 
 const AboutUs = () => (
 <div className="about_us" id="home">
+  <div className="about_us_content">
 
+    <div className="about_us_text">
+      <p className="our_story_text"> Our story</p>
+      <h1 className="about_us_header"> Welcome to Tawa Grill</h1>
+      <p className="about_us_p1"> Chef Patron Joan Smith and Executive Chef François Lemoine 
+         have teamed up to open one of the most exciting restaurants in Anytown.</p>
+      <p className="about_us_p2"> Our speciality is delicious cuisine created with the ingenuity and passion inspired by the fantastic flavours of France itself. 
+         We take great pride in improving upon your favourite French dishes in new and creative ways.</p>   
+    </div>
 
-  <div className="image_div">
-    <img src={images.about_us_image_1} className="image"></img>
+    <div className="image_1_div">
+      <img src={ images.about_us_image_4} alt="" className="image_1" />
+    </div>
+
+    <div className="image_2_div">
+      <img src={ images.about_us_image_5} alt="" className="image_2" />
+    </div>
+    
   </div>
-  <div className="text_div">
-    <h1 className="about_us_header_text">Eum ipsam laborum deleniti velit pariatur architecto aut nihil</h1>
-    <h2 className="about_us_header_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
-  </div>
-
-
-
-
-
-
-
-
-  </div>
+</div>
 );
 
 export default AboutUs;
