a4714f8a2d3e30be4314fa1d5614e583bfe04a80
Ichiro Banskota <36200378+Dumie101@users.noreply.github.com>
2024-06-28 16:32:26 -0300

    <10> About us
    Still working

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -6,25 +6,20 @@ import { images } from '../../constants';
 const AboutUs = () => (
 <div className="about_us" id="home">
 
-    <h1 className='about_us_header headtext__cormorant'>About Us</h1>
+
+  <div className="image_div">
+    <img src={images.about_us_image_1} className="image"></img>
+  </div>
+  <div className="text_div">
+    <h1 className="about_us_header_text">Eum ipsam laborum deleniti velit pariatur architecto aut nihil</h1>
+    <h2 className="about_us_header_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+  </div>
 
 
-    <div className="about_us_images">
-      <img src={images.about_us_image_2}/>
-      <img src={images.about_us_image_4}/>
-      <img src={images.about_us_image_3}/>
-    </div>
 
-    <p className="p__opensans" style={{margin: '2rem 10rem'}}>
-      Immerse yourself in a warm and inviting atmosphere at our restaurant. 
-      Savor mouthwatering dishes crafted with fresh, seasonal ingredients, 
-      showcasing the rich flavors of [insert cuisine type here]. 
-      Our friendly and attentive staff is dedicated to providing exceptional service, 
-      ensuring your dining experience is truly unforgettable. 
-      Whether you're celebrating a special occasion or simply enjoying a 
-      casual night out, we offer the perfect place to gather with loved ones 
-      and create lasting memories.
-    </p>
+
+
+
 
 
   </div>
